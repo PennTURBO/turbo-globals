310def400dc98665adb66ebfd09573551b1b19db
Mark A. Miller <mamillerpa@gmail.com>
2021-02-23 15:30:29 -0500

    orders_for_lot_setup.R for pennbiobank/turbo/orders_for_lot

---
+++ orders_for_lot_setup.R
@@ -0,0 +1,12 @@
+library(solrium)
+library(readxl)
+library(SPARQL)
+
+# try to limit these global setup files
+#   to package imports (using library())
+#   and to function definitions
+
+# the functions shouldn't really use any global variables,
+#   but I have done that in some cases
+
+print("from local file system")
